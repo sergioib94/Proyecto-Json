36102d3450caf190c5f7c6daee739f9812926e82
Sergio Ibañez <sergio_hd_sony@hotmail.com>
2020-02-21 18:51:46 +0100

    Ejercicio 1 hecho

---
+++ pokedex.py
@@ -0,0 +1,36 @@
+import json
+
+def Lista(datos):
+    num = []
+    pokedex = []
+    for id in datos["pokemon"]:
+        num.append(id["id"])
+
+    for info in datos["pokemon"]:
+        pokedex.append(info["name"])
+    return zip(num,pokedex)
+
+with open("pokedex.json") as fichero:
+    datos = json.load(fichero)
+
+while True:
+    print ("")
+    print ("Menu Principal:")
+    print ("")
+    print ("1.Listar Informacion: Mostrar los pokemons que hay.")
+    print ("2.Contar informacion: Contar pokemons que hay de un tipo especifico.")
+    print ("3.Buscar o Filtrar informacion: Buscar los pokemons que pueden salir de un huevo.")
+    print ("4.Buscar informacion relacionada: Meter por teclado una caracteristica de un pokemon y decir que pokemons superan esa estadistica.")
+    print ("5.Ejercicio Libre: Proponer un combate, pedir por teclado el nombre de dos pokemons y decir quien ganaria.")
+    print ("0.Salir")
+    print ("")
+
+    opcion = input("opcion: ")
+    print ("")
+
+    if opcion == "1":
+        for datos in Lista(datos):
+            print (datos[0],"-",datos[1])
+
+    if opcion == "0":
+        break;
